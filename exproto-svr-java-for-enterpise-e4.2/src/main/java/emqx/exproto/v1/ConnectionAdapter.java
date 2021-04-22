fe9739cdc0c6e4b8de5d76e229de102d96663c6a
wwhai <cnwwhai@gmail.com>
2021-04-22 11:03:50 +0800

    feat(demo): add java auth and publish demo

--- exproto-svr-java-for-enterpise-e4.2/src/main/java/emqx/exproto/v1/ConnectionAdapter.java
+++ exproto-svr-java-for-enterpise-e4.2/src/main/java/emqx/exproto/v1/ConnectionAdapter.java
@@ -10,11 +10,55 @@ import io.grpc.stub.StreamObserver;
 public class ConnectionAdapter extends ConnectionAdapterGrpc.ConnectionAdapterImplBase {
     @Override
     public void send(Exproto.SendBytesRequest request, StreamObserver<Exproto.CodeResponse> responseObserver) {
-        super.send(request, responseObserver);
+        System.out.println("[LOG] send");
+        responseObserver.onNext(Exproto.CodeResponse.getDefaultInstance());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void close(Exproto.CloseSocketRequest request, StreamObserver<Exproto.CodeResponse> responseObserver) {
+        System.out.println("[LOG] close");
+
+        super.close(request, responseObserver);
+    }
+
+    @Override
+    public void authenticate(Exproto.AuthenticateRequest request, StreamObserver<Exproto.CodeResponse> responseObserver) {
+        System.out.println("[LOG] authenticate");
+
+        responseObserver.onNext(Exproto.CodeResponse.getDefaultInstance());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void startTimer(Exproto.TimerRequest request, StreamObserver<Exproto.CodeResponse> responseObserver) {
+        System.out.println("[LOG] startTimer");
+
+        responseObserver.onNext(Exproto.CodeResponse.getDefaultInstance());
+        responseObserver.onCompleted();
     }
 
     @Override
     public void publish(Exproto.PublishRequest request, StreamObserver<Exproto.CodeResponse> responseObserver) {
-        super.publish(request, responseObserver);
+        System.out.println("[LOG] publish");
+
+        responseObserver.onNext(Exproto.CodeResponse.newBuilder().buildPartial());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void subscribe(Exproto.SubscribeRequest request, StreamObserver<Exproto.CodeResponse> responseObserver) {
+        System.out.println("[LOG] subscribe");
+
+        responseObserver.onNext(Exproto.CodeResponse.getDefaultInstance());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void unsubscribe(Exproto.UnsubscribeRequest request, StreamObserver<Exproto.CodeResponse> responseObserver) {
+        System.out.println("[LOG] unsubscribe");
+
+        responseObserver.onNext(Exproto.CodeResponse.getDefaultInstance());
+        responseObserver.onCompleted();
     }
 }
